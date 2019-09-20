573668f99b3e71b4c3549286c28efb55ad914bb8
tbvescio <43358562+tbvescio@users.noreply.github.com>
2019-09-20 20:12:52 -0300

    Add files via upload

--- db.py
+++ db.py
@@ -1,29 +1,60 @@
 import sqlite3
 
-con = sqlite3.connect('users.db')
+con = sqlite3.connect('users.db', check_same_thread=False)
 elidart = con.cursor()
 
 def create_table():
+	con = sqlite3.connect('users.db', check_same_thread=False)
 	elidart = con.cursor()
 	elidart.execute("CREATE TABLE IF NOT EXISTS users(id integer PRIMARY KEY, email text, name text, user text, password text)")
-	con.commit()
-	
-def save_data(email,name,username,password):
-	con = sqlite3.connect('users.db')
-	x =  f"INSERT INTO users(email,name,user,password) values('{email}','{name}','{username}','{password}')"
-	elidart.execute(x)
+	elidart.execute("CREATE TABLE IF NOT EXISTS messages(id integer PRIMARY KEY, message text)")
 	con.commit()
 	con.close()
-	return True
+	return
+
+def save_data(email,name,username,password):
+	con = sqlite3.connect('users.db', check_same_thread=False)
+	elidart = con.cursor()
+	query = f"SELECT * FROM users WHERE email = '{email}' or user = '{username}'"
+	if elidart.execute(query).fetchall() == []: #si no hay resultados con esos parametros
+		query=  f"INSERT INTO users(email,name,user,password) values('{email}','{name}','{username}','{password}')"
+		elidart.execute(query)
+		con.commit()
+		con.close()
+		return
+	else: #si ya existe un usuario registrado 
+		return False
+
+def get_messages():
+	con = sqlite3.connect('users.db', check_same_thread=False)
+	query = elidart.execute("SELECT * FROM messages").fetchall() #obtiene todos los registros
+	messages = []
+	for row in query:
+		messages.append(row[1]) #guarda los mensajes en una lista
+	con.commit()
+	con.close()
+	return messages
+
+def save_messages(msg): #guarda los mensajes en la tabla
+	con = sqlite3.connect('users.db', check_same_thread=False)
+	elidart = con.cursor()
+	query=  f"INSERT INTO messages(message) values('{msg}')"
+	elidart.execute(query)
+	con.commit()
+	con.close()
+	return
+
 
 def check_data(username,password):
-	con = sqlite3.connect('users.db')
-	x = f"select * from users where user = '{username}' and password = '{password}'"
-	elidart.execute(x)
-	result = elidart.fetchall()
+	con = sqlite3.connect('users.db', check_same_thread=False)
+	elidart = con.cursor()
+	query = f"select * from users where user = '{username}' and password = '{password}'"
+	elidart.execute(query)
+	result = elidart.fetchall() #muestra resultados como lista
 	con.commit()
 	con.close()
-	if result == []:
+	if result == []: #si no hay querys con esos parametros
 		return False
 	else:
 		return True
+
--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_socketio import SocketIO
-from db import save_data, check_data, create_table
+from db import save_data, check_data, create_table, save_messages,get_messages
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'UBH6sOYUiF'
 socketio = SocketIO(app) #capsule flask app in socket
 
+
 create_table()
 
 @app.route('/')
 def home():
-    
     if 'username' in session: #if there is an user logged in 
-        return render_template('index.html', username=session['username'])
+        
+        #when the cliente requests the page get the messages
+        messages = get_messages()
+       
+        return render_template('index.html', username=session['username'], messages=messages)
     else: #if not makes them logged in 
         return redirect('/login')
 
-
 def recived_message(methods=['GET','POST']):
     print("Message recived!")
+
+
+
+@socketio.on('event_message') #happend when recives the event
+def handle_event(json, methods=['GET','POST']):
+    print("event: " + str(json))
+
+    #when receives the messages stores it in the db
+    save_messages(json['user_name'] + ":" + json['message'])
+
+    socketio.emit('response', json, callback=recived_message) #callback is debug method
 
 
 
@@ -51,9 +65,10 @@ def registro():
         if email=="" or name=="" or user=="" or password=="": #if there is empty fields
             return render_template('register.html', error='Faltan datos!') 
 
-        save_data(email,name,user,password)
-        return redirect('/login')
-
+        if save_data(email,name,user,password) == False:
+            return render_template('register.html', error='Datos ya registrados!') 
+        else:
+            return redirect('/login')
 
 
 @app.route('/logout')
@@ -63,10 +78,6 @@ def logout():
     return redirect('/') 
 
 
-@socketio.on('event_message') #happend when recives the event
-def handle_event(json, methods=['GET','POST']):
-    print("event: " + str(json))
-    socketio.emit('response', json, callback=recived_message) #callback is debug method
 
 
 if __name__ == '__main__':
