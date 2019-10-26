c88c48f1db5e1342002a958747a28041d5a5811b
SanFre10 <52258528+SanFre10@users.noreply.github.com>
2019-10-26 17:52:49 -0300

    CSS

--- static/script.js
+++ static/script.js
@@ -2,49 +2,49 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 
 //Cuando un usuario se conecta
-socket.on('connect', function () {
+socket.on('connect', function() {
 
     $('div.mensajes').scrollTop(100000)
 
     //Al hacer clkic en enviar mensaje
-    $('form').on('submit', function (e) {
+    $('form').on('submit', function(e) {
 
         e.preventDefault()
 
         var user_name = nombre
-        var user_input = $('input.message').val()
+        var user_input = $('input.message_i').val()
 
         //Si el mensaje no esta vacio
         if (user_input != "") {
 
             //Envia nombre de usuario y mensaje al servidor
             socket.emit('event_message', {
-                user_name: user_name, message: user_input
+                user_name: user_name,
+                message: user_input
             })
         }
         //Limpia el input del mensaje
-        $('input.message').val('').focus()
+        $('input.message_i').val('').focus()
     })
 })
 
 //Imprime el mensaje enviado
-socket.on('response', function (msg) {
+socket.on('response', function(msg) {
 
-    $('div.mensajes').append('<hr class="linea">')
-    $('div.mensajes').append('<p><b>' + msg.user_name + ':</b><br>' + msg.message + '</p>')
+    $('div.mensajes').append('<p class="message"><b>' + msg.user_name + ':</b><br>' + msg.message + '</p>')
 
     //Scrolea hasta el final
     $('div.mensajes').scrollTop(100000)
 })
 
 //Cuando un usuario se conecta o desconecta actualiza la lista de usuarios conectadps
-socket.on('users', function (log) {
+socket.on('users', function(log) {
 
     //Vacia la lista logueados
     $('div.usuarios').empty()
 
     //Por cada item en la lista
-    $.each(log, function () {
+    $.each(log, function() {
         $('div.usuarios').append('<p class="logged_user">' + this + '</p>')
     })
 })
